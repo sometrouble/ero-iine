119a54578c6a03352b3ea16541df63d4beeb4f9e
sometrouble <sumeshi008@gmail.com>
2018-12-23 05:45:35 +0900

    [build] re-re-re-build fuck you hash mark fuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu

--- src/router.js
+++ src/router.js
@@ -5,8 +5,8 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL + "ero-iine",
+  mode: 'hash',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
