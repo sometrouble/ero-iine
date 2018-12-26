77bfedfdb4cf7da4c0d546a03e8d6d9904807d59
sometrouble <sumeshi008@gmail.com>
2018-12-27 08:24:04 +0900

    [add] tweet function

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@
       v-btn(flat to="gallery")
         span.mr-2 latest Ero-iine(Beta)
         v-icon fas fa-external-link-alt
+      v-btn(small flat icon @click="tweet")
+        v-icon fab fa-twitter
     v-content
       router-view
     v-footer.ml-3
@@ -37,6 +39,22 @@ export default {
         },
       ],
     }
+  },
+  methods: {
+    tweet() {
+      let EUC = encodeURIComponent;
+      let LINK = window.location.href;
+      let message = "エロいいね研究会 - Sexual Favorite Research Society of Japan."
+      let hashtag = "エロいいね研究会, SFRS-jp"
+
+      let URL = `https://twitter.com/intent/tweet?text=${EUC(message)}&hashtags=${EUC(hashtag)}&url=${LINK}`
+
+      if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+          location.href = URL
+      }else{
+          window.open(URL, "_blank","top=50,left=50,width=500,height=500,scrollbars=1,location=0,menubar=0,toolbar=0,status=1,directories=0,resizable=1");
+      }
+    },
   }
 }
 </script>
