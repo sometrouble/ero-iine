9a861f02aa6ab286655944fe0083d23a64b576f8
sometrouble <sumeshi008@gmail.com>
2019-01-02 10:31:38 +0900

    [add] tag function

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -13,8 +13,8 @@
               blockquote.twitter-tweet(width="300" height="300")
                 a(:href="i[0]")
             v-flex(lg6)
-              div.mr-3.ml-3(v-for="(t, index) in tag")
-                v-chip(@input="tag.splice([index], 1)" small color="primary" close label outline style="width:100%;")
+              div.mr-3.ml-3(v-for="(t, tindex) in i[3].split(',')")
+                v-chip(v-if="t" @input="remove_tag(index,t)" small color="primary" close label outline style="width:100%;")
                   v-icon(left) label
                   | {{ t }}
               v-dialog(max-width="600px")
@@ -28,7 +28,7 @@
                       v-text-field(label="tag name" v-model="formdata.tag" required :rules="[v => !!v || 'required field']")
                     v-card-actions
                       v-spacer
-                      v-btn(:disabled="!valid" color="primary" @click="add_tag") Add tag
+                      v-btn(:disabled="!valid" color="primary" @click="add_tag(index)") Add tag
 </template>
 
 <script>
@@ -39,12 +39,12 @@
         eroiine: [],
         components: {
         },
-        tag: [
-          'タグ機能は',
-          '未実装です',
-          'エロいいね研究会を',
-          'よろしくお願いいたします',
-        ],
+        //tag: [
+        //  'タグ機能は',
+        //  '未実装です',
+        //  'エロいいね研究会を',
+        //  'よろしくお願いいたします',
+        //],
         valid: true,
         formdata: {
           tag: '',
@@ -65,12 +65,34 @@
         axios.get('https://script.google.com/macros/s/AKfycbxaaMlZxtrSBDJNxFwv6TJbhV32U7pa39p_4sjjRhS69HHXKFPu/exec')
           .then((response) => {
             let temp = response.data.data;
-            this.eroiine = temp.reverse().slice(0,32);
+            this.eroiine = temp.reverse().slice(0,30);
           });
       },
-      add_tag() {
-        this.tag.push(this.formdata.tag);
+      add_tag(index) {
+        let newtag = this.eroiine[index][3] + "," + this.formdata.tag
+
+        var params = new URLSearchParams();
+        params.append('action', "add");
+        params.append('url', this.eroiine[index][0]);
+        params.append('tag', newtag);
+
+        axios.post('https://script.google.com/macros/s/AKfycbz0D8DfjgGYiTAs1bED1wBstFRvuUP6wfbuEJ42Ed74uLEKiYk/exec', params)
+          .then((response) => {});
+
+        this.eroiine[index][3] = newtag;
         this.formdata.tag = '';
+      },
+      remove_tag(index, tagname) {
+        var params = new URLSearchParams();
+        params.append('action', "remove");
+        params.append('url', this.eroiine[index][0]);
+        params.append('tag', tagname);
+
+        axios.post('https://script.google.com/macros/s/AKfycbz0D8DfjgGYiTAs1bED1wBstFRvuUP6wfbuEJ42Ed74uLEKiYk/exec', params)
+          .then((response) => {
+            let newtag = response.data.newtag;
+            this.eroiine[index][3] = newtag;
+          });
       }
     },
     mounted() {
